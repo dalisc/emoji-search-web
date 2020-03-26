7c715536dd455c855bb4af8de1bf26c62d53b320
Dalis Chan <42921826+dalisc@users.noreply.github.com>
2020-03-26 18:59:16 +0800

    Formatcode with prettier

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,43 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
-import "./SearchBar.css";
+import './SearchBar.css';
 
 export default class SearchBar extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-          query: ""
+            query: '',
         };
-      }
+    }
 
-  static propTypes = {
-    textChange: PropTypes.func
-  };
+    static propTypes = {
+        textChange: PropTypes.func,
+    };
 
-  handleChange = event => {
-    this.props.textChange(event);
-  };
+    handleChange = (event) => {
+        this.props.textChange(event);
+    };
 
-  render() {
-    return (
-      <div className="component-search-input">
-        <div>
-          <input 
-            placeholder="Search for your emoji..."
-            onChange={event => this.handleChange(event)}
-            onKeyPress={event => {
-                        if (event.key === 'Enter') {
-                          this.handleChange(event)
-                        }
-                      }} />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "component-search-input" >
+            <
+            div >
+            <
+            input placeholder = "Search for your emoji..."
+            onChange = {
+                (event) => this.handleChange(event) }
+            onKeyPress = {
+                (event) => {
+                    if (event.key === 'Enter') {
+                        this.handleChange(event);
+                    }
+                }
+            }
+            /> <
+            /div> <
+            /div>
+        );
+    }
 }
--- src/container/App.js
+++ src/container/App.js
@@ -7,7 +7,7 @@ import BeatLoader from "react-spinners/BeatLoader";
 import './App.css';
 import CategoriesSelect from "../components/CategoriesSelect";
 import axios from 'axios';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 
 const API_KEY = process.env.OPEN_EMOJI_API_KEY;
@@ -15,116 +15,128 @@ var CancelToken = axios.CancelToken;
 let cancel;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      category: 'All',
-      isLoading: true,
-      filteredEmoji: []
-    };
-  }
-
-  async componentDidMount() {
-    document.title = 'Your page title here';
-
-    // Load all emojis
-    let response = await API.get('/emojis?&access_key=' + API_KEY);
-    console.log(response);
-
-    const filteredEmoji = response.data;
-
-    this.setState({
-      ...this.state, ...{
-        isLoading: false,
-        filteredEmoji
-      }
-    });
-  }
-
-  // Filter emojis when category is changed
-  setCategory = selected => {
-    var category;
-    if (selected == null) {
-      category = 'All';
-    } else {
-      category = selected.label;
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+            category: 'All',
+            isLoading: true,
+            filteredEmoji: []
+        };
     }
-    this.setState({category: category});
-    this.handleSearchChange();
-  }
 
-  handleSearchChange = event => {
+    async componentDidMount() {
 
-    if (cancel != undefined) {
-      cancel();
+        // Load all emojis
+        let response = await API.get('/emojis?&access_key=' + API_KEY);
+        console.log(response);
 
+        const filteredEmoji = response.data;
+
+        this.setState({
+            ...this.state,
+            ... {
+                isLoading: false,
+                filteredEmoji,
+            }
+        });
     }
-    this.setState({
-      isLoading: true,
-    })
 
-    if (event != undefined) {
-      this.setState({
-        query: event.target.value
-      })
-    }
-    
-    console.log("Searching for emojis...\n");
-    API.get('/emojis?search=' + this.state.query + '&access_key=' + API_KEY, {
-      cancelToken: new CancelToken(function executor(c) {
-        cancel = c;
-      })
-    })
-      .then(response => {
-        if (response.data == null) {
-          this.setState({ 
-            isLoading: false,
-            filteredEmoji: []}, () => 
-            console.log(response))
+    // Filter emojis when category is changed
+    setCategory = selected => {
+        var category;
+        if (selected == null) {
+            category = 'All';
         } else {
-          this.setState({ 
-            isLoading: false,
-            filteredEmoji: this.state.category == 'All'? response.data : response.data.filter((data) => data.group.includes(this.state.category))}, () => 
-            console.log(response))
-          }
+            category = selected.label;
         }
-      ). catch(function(thrown) {
-        if (axios.isCancel(thrown)) {
-          console.log('Request canceled', thrown.message);
+        this.setState({ category: category });
+        this.handleSearchChange();
+    }
+
+    handleSearchChange = event => {
+
+        if (cancel != undefined) {
+            cancel();
+
         }
-      })
-  };
+        this.setState({
+            isLoading: true,
+        })
 
-  render() {
+        if (event != undefined) {
+            this.setState({
+                query: event.target.value
+            })
+        }
 
-    const { isLoading, filteredEmoji } = this.state;
-    return (
-      <div className="App">
-                    <Helmet>
-                <meta charSet="utf-8" />
-                <title>Emoji Search</title>
-            </Helmet>
-        <div className="header">
-          <Header />
-        </div>
+        console.log("Searching for emojis...\n");
+        API.get('/emojis?search=' + this.state.query + '&access_key=' + API_KEY, {
+                cancelToken: new CancelToken(function executor(c) {
+                    cancel = c;
+                })
+            })
+            .then(response => {
+                if (response.data == null) {
+                    this.setState({
+                            isLoading: false,
+                            filteredEmoji: []
+                        }, () =>
+                        console.log(response))
+                } else {
+                    this.setState({
+                            isLoading: false,
+                            filteredEmoji: this.state.category == 'All' ? response.data : response.data.filter((data) => data.group.includes(this.state.category))
+                        }, () =>
+                        console.log(response))
+                }
+            }).catch(function(thrown) {
+                if (axios.isCancel(thrown)) {
+                    console.log('Request canceled', thrown.message);
+                }
+            })
+    };
 
-        <div className="search">
-          <div className="searchbox">
-            <SearchBar 
-              textChange={this.handleSearchChange}/>
-          </div>
-          <div className="category">
-            <CategoriesSelect 
-              categoryChange={this.setCategory}/>
-          </div>
-        </div>
-        
-       {isLoading ? <div className="loader"><BeatLoader /></div> : <div className="results"><Results emojiData={filteredEmoji} /></div>}
-        
-      </div>
-    );  
-  }
-}
+    render() {
 
-export default App;
+        const { isLoading, filteredEmoji } = this.state;
+        return ( <
+            div className = "App" >
+            <
+            Helmet >
+            <
+            meta charSet = "utf-8" / >
+            <
+            title > Emoji Search < /title> <
+            /Helmet> <
+            div className = "header" >
+            <
+            Header / >
+            <
+            /div>
+
+            <
+            div className = "search" >
+            <
+            div className = "searchbox" >
+            <
+            SearchBar textChange = { this.handleSearchChange }
+            /> <
+            /div> <
+            div className = "category" >
+            <
+            CategoriesSelect categoryChange = { this.setCategory }
+            /> <
+            /div> <
+            /div>
+
+            {
+                isLoading ? < div className = "loader" > < BeatLoader / > < /div> : <div className="results"><Results emojiData={filteredEmoji} / > < /div>}
+
+                <
+                /div>
+            );
+        }
+    }
+
+    export default App;
